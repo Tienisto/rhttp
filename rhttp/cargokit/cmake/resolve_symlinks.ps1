1ab7f3ae08b270902a8645e3bbbb5471f050e1a6
舰队的偶像-岛风酱! <frg2089@outlook.com>
2024-12-23 23:18:49 +0800

    Update resolve_symlinks.ps1

--- rhttp/cargokit/cmake/resolve_symlinks.ps1
+++ rhttp/cargokit/cmake/resolve_symlinks.ps1
@@ -8,7 +8,8 @@ function Resolve-Symlinks {
 
     $Private:Item = Get-Item -LiteralPath $Path
     if ([string]::IsNullOrEmpty($Private:Item.Target)) {
-        return Resolve-Symlinks $Private:Item.Parent
+        $Private:Parent = Resolve-Symlinks $Private:Item.Parent
+        return Join-Path $Private:Parent $Private:Item.Name
     }
     else {
         return $Private:Item.Target
