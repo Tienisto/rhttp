704c5b14f49dd3f9344b45bc1a5b0911b76358a6
舰队的偶像-岛风酱! <frg2089@outlook.com>
2025-05-31 10:49:52 +0800

    Update resolve_symlinks.ps1
    See irondash/cargokit#90

--- rhttp/cargokit/cmake/resolve_symlinks.ps1
+++ rhttp/cargokit/cmake/resolve_symlinks.ps1
@@ -6,15 +6,29 @@ function Resolve-Symlinks {
         [string] $Path
     )
 
-    $Private:Item = Get-Item -LiteralPath $Path
-    if ([string]::IsNullOrEmpty($Private:Item.Target)) {
-        $Private:Parent = Resolve-Symlinks $Private:Item.Parent
-        return Join-Path $Private:Parent $Private:Item.Name
+    [string] $separator = '/'
+    [string[]] $parts = $Path.Split($separator)
+
+    [string] $realPath = ''
+    foreach ($part in $parts) {
+        if ($realPath -and !$realPath.EndsWith($separator)) {
+            $realPath += $separator
+        }
+
+        $realPath += $part.Replace('\', '/')
+
+        # The slash is important when using Get-Item on Drive letters in pwsh.
+        if (-not($realPath.Contains($separator)) -and $realPath.EndsWith(':')) {
+            $realPath += '/'
+        }
+
+        $item = Get-Item $realPath
+        if ($item.LinkTarget) {
+            $realPath = $item.LinkTarget.Replace('\', '/')
+        }
     }
-    else {
-        return $Private:Item.Target
-    }
+    $realPath
 }
 
-$path=Resolve-Symlinks -Path $args[0]
+$path = Resolve-Symlinks -Path $args[0]
 Write-Host $path
